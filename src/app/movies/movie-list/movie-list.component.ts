7737e76d2efce9fed0776e179eac27b567f8ec8d
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-18 13:22:37 +0300

    feat : make movie-list an ul element

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../movies.service';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.sass']
+  styleUrls: ['./movie-list.component.sass'],
 })
 export class MovieListComponent implements OnInit {
-
-  constructor() { }
+  movieList = [];
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.moviesService.movieSearchUpdated.subscribe(() => {
+      this.movieList = this.moviesService.getMovieSearch();
+      console.log(this.movieList);
+    });
   }
-
 }
