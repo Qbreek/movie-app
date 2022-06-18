5ac776fc8658f80d2a5664fc211fcca7d2cbcea5
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-18 15:38:07 +0300

    new path

---
+++ src/app/shared/movie.model.ts
@@ -0,0 +1,13 @@
+export class Movie {
+  constructor(
+    public title: string,
+    public plot: string,
+    public year: string,
+    public runtime: string,
+    public genre: string,
+    public director: string,
+    public actors: string,
+    public rating: string,
+    public poster: string
+  ) {}
+}
