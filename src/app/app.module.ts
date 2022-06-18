62e85216f852f9c04a9e26c74de924a19de5902c
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-18 13:18:56 +0300

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
+//Angular imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
+//Material UI imports
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+//Component imports
+import { AppComponent } from './app.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { ProfileComponent } from './profile/profile.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieListComponent } from './movies/movie-list/movie-list.component';
 import { MovieItemComponent } from './movies/movie-list/movie-item/movie-item.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
@@ -25,21 +30,23 @@ import { MovieItemComponent } from './movies/movie-list/movie-item/movie-item.co
     DiscoverComponent,
     ProfileComponent,
     WatchlistComponent,
-    NavBarComponent,
     MoviesComponent,
     MovieListComponent,
     MovieItemComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatIconModule,
     MatButtonModule,
     MatInputModule,
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
