f266f5868a26ec5b19064b4cbb5a45eb5a2352e0
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-18 13:23:02 +0300

    minor tweaks

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../movies/movies.service';
 
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.component.html',
-  styleUrls: ['./discover.component.sass']
+  styleUrls: ['./discover.component.sass'],
 })
 export class DiscoverComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
