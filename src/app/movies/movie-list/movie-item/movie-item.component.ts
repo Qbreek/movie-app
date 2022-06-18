d0017c8943f20825cfd6e94bb9ce92cd731d5d44
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-18 13:19:39 +0300

    feat : restyle and restructure movie item

--- src/app/movies/movie-list/movie-item/movie-item.component.ts
+++ src/app/movies/movie-list/movie-item/movie-item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/shared/movie.model';
 
 @Component({
   selector: 'app-movie-item',
   templateUrl: './movie-item.component.html',
-  styleUrls: ['./movie-item.component.sass']
+  styleUrls: ['./movie-item.component.sass'],
 })
 export class MovieItemComponent implements OnInit {
+  @Input() movie: Movie;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
