9b457402e916fca2c9c8c5d6c594215ecb7f35ea
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-19 13:15:36 +0300

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 //Component imports
 import { AppComponent } from './app.component';
@@ -19,10 +21,14 @@ import { DiscoverComponent } from './discover/discover.component';
 import { ProfileComponent } from './profile/profile.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MoviesComponent } from './movies/movies.component';
-import { MovieListComponent } from './movies/movie-list/movie-list.component';
-import { MovieItemComponent } from './movies/movie-list/movie-item/movie-item.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MovieDisplayComponent } from './movie-display/movie-display.component';
+import { NavigationComponent } from './toolbar/navigation/navigation.component';
+import { SearchComponent } from './filters/search/search.component';
+import { FiltersComponent } from './filters/filters.component';
+
+//Pipes
 import { TrimOver20CharPipe } from './shared/trim-over20-char.pipe';
 
 @NgModule({
@@ -31,11 +37,13 @@ import { TrimOver20CharPipe } from './shared/trim-over20-char.pipe';
     DiscoverComponent,
     ProfileComponent,
     WatchlistComponent,
-    MoviesComponent,
-    MovieListComponent,
     MovieItemComponent,
     ToolbarComponent,
     TrimOver20CharPipe,
+    MovieDisplayComponent,
+    NavigationComponent,
+    SearchComponent,
+    FiltersComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,6 +57,8 @@ import { TrimOver20CharPipe } from './shared/trim-over20-char.pipe';
     MatListModule,
     MatCardModule,
     MatToolbarModule,
+    MatTabsModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
