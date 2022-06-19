a5357df18a5659d04e46ce2e720c5f38f8a849a1
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-19 13:14:10 +0300

    feat : add movie display component

--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Movie } from '../shared/movie.model';
+import { WatchlistService } from '../shared/watchlist.service';
 
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
-  styleUrls: ['./watchlist.component.sass']
+  styleUrls: ['./watchlist.component.sass'],
 })
-export class WatchlistComponent implements OnInit {
-
-  constructor() { }
+export class WatchlistComponent implements OnInit, OnDestroy {
+  watchlist: Movie[] = [];
+  watchlistSubscription: Subscription;
+  constructor(private watchlistService: WatchlistService) {}
 
   ngOnInit(): void {
+    this.watchlistSubscription = this.watchlistService
+      .getWatchlist()
+      .subscribe((response) => {
+        this.watchlist = response;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.watchlistSubscription.unsubscribe();
+  }
 }
