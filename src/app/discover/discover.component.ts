587132d261eb81853d2abc544f61512a598807ab
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-19 13:14:26 +0300

    feat : add display component

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { MoviesService } from '../movies/movies.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Movie } from '../shared/movie.model';
+import { OmdbService } from '../shared/omdbService.service';
 
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.component.html',
   styleUrls: ['./discover.component.sass'],
 })
-export class DiscoverComponent implements OnInit {
-  constructor() {}
+export class DiscoverComponent implements OnInit, OnDestroy {
+  searchList: Movie[] = [];
+  movieSearchSubscription: Subscription;
 
-  ngOnInit(): void {}
+  constructor(private omdbService: OmdbService) {}
+
+  ngOnInit(): void {
+    this.searchList = this.omdbService.getMovieSearch();
+    this.movieSearchSubscription =
+      this.omdbService.movieSearchUpdated.subscribe(() => {
+        this.searchList = this.omdbService.getMovieSearch();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.movieSearchSubscription.unsubscribe();
+  }
 }
