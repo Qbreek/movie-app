b3edc52bdcbce70a10aa350da745946ec91c3e36
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-21 18:19:44 +0300

    to be continued

---
+++ src/app/movie-item/button-display/button-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-display',
+  templateUrl: './button-display.component.html',
+  styleUrls: ['./button-display.component.sass']
+})
+export class ButtonDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
