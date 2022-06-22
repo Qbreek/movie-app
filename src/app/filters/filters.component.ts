56afcfb69dff8b5196d807a5e36a59cddeef1998
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-22 15:11:41 +0300

    feat : each function informs the application about the filter selected

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-filters',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters.component.sass'],
 })
 export class FiltersComponent implements OnInit {
-  constructor() {}
+  appliedFilter: string = '';
+  constructor(private firebaseService: FirebaseService) {}
+
   ngOnInit(): void {}
+
+  byRating() {
+    this.firebaseService.sortOccured.next('rating');
+    this.appliedFilter = 'Rating';
+  }
+
+  byRelease() {
+    this.firebaseService.sortOccured.next('year');
+    this.appliedFilter = 'Release Date';
+  }
+
+  byRuntime() {
+    this.firebaseService.sortOccured.next('runtime');
+    this.appliedFilter = 'Runtime';
+  }
 }
