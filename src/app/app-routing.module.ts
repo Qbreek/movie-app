91288a110d106bbcefdea6c94a1f0c641f51d43d
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-06-22 15:08:02 +0300

    feat : add archive route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArchiveComponent } from './archive/archive.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { ProfileComponent } from './profile/profile.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'discover', component: DiscoverComponent },
   { path: 'watchlist', component: WatchlistComponent },
+  { path: 'archive', component: ArchiveComponent },
 ];
 
 @NgModule({
